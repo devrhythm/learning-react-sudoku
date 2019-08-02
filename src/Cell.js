305427b068631c8c921d63b5d2e2b290d6d5b87a
SUMETH KONGPRASERT <sumeth.kongprasert2@gmail.com>
2019-08-02 20:18:37 +0700

    use functional component. note: cannot use state.

--- src/Cell.js
+++ src/Cell.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Cell extends Component {
-  render() {
-    return (
-      <div
-        onClick={e => {
-          if (this.props.isInitial) {
-            return;
-          }
-          this.props.onChange((this.props.number + 1) % 5);
-        }}
-        className={`cell ${this.props.isInitial ? "initial" : ""}`}
-      >
-        {this.props.number !== 0 && this.props.number}
-      </div>
-    );
-  }
-}
+const Cell = (props) => (
+  <div
+    onClick={e => {
+      if (props.isInitial) {
+        return;
+      }
+      props.onChange((props.number + 1) % 5);
+    }}
+    className={`cell ${props.isInitial ? "initial" : ""}`}
+  >
+    {props.number !== 0 && props.number}
+  </div>
+);
 
 export default Cell;
